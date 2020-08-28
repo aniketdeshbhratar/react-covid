37e399172736d5dd521e529e07c538a76aee973f
Aniket Deshbhratar <aniket@madasolns.com>
2020-08-28 23:03:52 +0530

    Base setup

---
+++ covid2/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets try this</h1>
+
+      {/* Header */}
+      {/* Title + Seclect input drop down field */}
+
+      {/* inforboxes */}
+      {/* inforboxes */}
+      {/* inforboxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
